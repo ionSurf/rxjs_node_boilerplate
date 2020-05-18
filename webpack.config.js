56859ce5ce398921c9ec2249d8e18309c27b3409
Javier <javier@Jessicas-MacBook-Pro.local>
2020-05-18 17:49:46 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	entry: './src/app.js',
+	output: {
+		path: __dirname + '/dist',
+		filename:'app.bundle.js'
+	},
+	module: {
+		rules: [{
+			exclude: '/node_modules/',
+			loader: 'babel-loader',
+			query: {
+				presets: ['es2015']
+			}
+		}]
+	}
+}
